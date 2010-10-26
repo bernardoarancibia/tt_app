19b2b378b2efca2c8f7bd5f959128f2895e8e617
Sebastian Machuca <sebastian.machuca@alumnos.usm.cl>
2010-10-26 14:36:36 -0300

    agrego datos a seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-    Categoria.create( { :nombre => 'frutas'} , { :nombre => 'aseo'} )
+    Categoria.create([ { :nombre => 'frutas'} , { :nombre => 'aseo'} ])
