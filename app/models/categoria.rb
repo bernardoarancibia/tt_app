69b393517e933cc4d5a89b00f0ce2b53decb3550
Sebastian Machuca <sebastian.machuca@alumnos.usm.cl>
2010-10-26 14:50:05 -0300

    agrego validaciones y relaciones a modelo categoria

--- app/models/categoria.rb
+++ app/models/categoria.rb
@@ -1,2 +1,26 @@
+#encoding: utf-8
 class Categoria < ActiveRecord::Base
+  
+  #Dependencia existencial
+    has_many :productos
+  
+  #Atributos accesibles
+    attr_accessible :nombre
+  
+  #Antes de validar
+    before_validation :downcase_nombre
+  
+  #Validaciones
+    validates_presence_of :nombre
+    validates_uniqueness_of :nombre
+    validates_length_of :nombre, :maximum => 40    
+    validates_format_of :nombre, :with => /^([a-zA-Z áéíóúñÁÉÍÓÚÜÑ]{1,})([a-zA-ZñáéíóúÁÉÍÓÚÜüÑ])$/
+  
+  #Metodos
+    protected #---------------------
+    
+    def downcase_nombre
+      self.nombre.downcase!
+    end
+
 end
