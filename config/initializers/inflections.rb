d0c00f36d75835afd8608d975aa7b3c4459889c4
Bernardo Arancibia <debiano@gmail.com>
2010-10-26 13:45:02 -0300

    Configurando Rails 3 para Español | initializers, config

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,3 +8,19 @@
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
+
+# encoding: utf-8
+# Be sure to restart your server when you modify this file.
+# Add new inflection rules using the following format
+# (all these examples are active by default):
+ActiveSupport::Inflector.inflections do |inflect|
+inflect.plural /([aeiou])([A-Z]|_|$)/, '\1s\2'
+inflect.plural /([rlnd])([A-Z]|_|$)/, '\1es\2'
+inflect.singular /([aeiou])s([A-Z]|_|$)/, '\1\2'
+inflect.singular /([rlnd])es([A-Z]|_|$)/, '\1\2'
+inflect.irregular 'user', 'users'
+inflect.irregular 'account', 'accounts'
+inflect.irregular 'password', 'passwords'
+inflect.irregular 'session', 'sessions'
+end
+
