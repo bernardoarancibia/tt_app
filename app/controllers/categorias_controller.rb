9eaa0c9de0a9f993d5f9bcc2d1fadb937a08a222
Sebastian Machuca <sebastian.machuca@alumnos.usm.cl>
2010-10-26 15:28:34 -0300

    agrego archivos haml de categoria

--- app/controllers/categorias_controller.rb
+++ app/controllers/categorias_controller.rb
@@ -1,2 +1,35 @@
+#encoding: utf-8
 class CategoriasController < ApplicationController
+  
+  def index
+    @categorias = Categoria.order(:nombre)
+  end
+
+  def list
+  end  
+
+  def new
+    @categoria = Categoria.new  
+  end
+
+  def create
+    @categoria = Categoria
+  end
+
+  def edit
+     
+  end
+
+  def update
+    
+  end  
+
+  def destroy
+  
+  end
+
+  def productos
+
+  end
+
 end
