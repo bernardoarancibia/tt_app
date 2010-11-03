79f7800493c6e749c4f1472c956aa94ef72c3788
Bernardo Arancibia <debiano@gmail.com>
2010-11-03 02:37:20 -0300

    Primera versión completa mantenedor Productos, SIN PAPERCLIP

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -1,7 +1,50 @@
 class ProductosController < ApplicationController
 
+  before_filter :find_producto, :only => [:edit, :update, :destroy]
+  before_filter :find_proveedores_categorias, :only => [:new, :create, :edit, :update]
+
   def index
     @productos = Producto.order(:nombre)
+  end
+
+  def new
+    @producto = Producto.new
+  end
+
+  def create
+    @producto = Producto.new(params[:producto])
+    if @producto.save
+      redirect_to :productos, :notice => 'El producto fue ingresado exitosamente.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @producto.update_attributes(params[:producto])
+      redirect_to :productos, :notice => 'El producto fue actualizado exitosamente.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @producto.destroy
+    redirect_to :productos, :notice => 'El producto fue eliminado exitosamente.'
+  end
+
+  private #----
+
+  def find_producto
+    @producto = Producto.find_by_id(params[:id])
+  end
+
+  def find_proveedores_categorias
+    @proveedores = Proveedor.order(:nombre)
+    @categorias = Categoria.order(:nombre)
   end
 
 end
--- app/models/producto.rb
+++ app/models/producto.rb
@@ -1,6 +1,44 @@
+#encoding: utf-8
+
 class Producto < ActiveRecord::Base
 
   belongs_to :categoria
   belongs_to :proveedor
+
+  before_validation :downcase_nombre
+
+  validates :nombre, :length => { :maximum => 40 }
+  validates_format_of :nombre,
+    :with => /^([A-Za-z0-9ÁÉÍÓÚáéíóúÑÜñü]{1,})([ A-Za-z0-9ÁÉÍÓÚáéíóúÑÜñü\/&-]{1,})$/, :allow_nil => true
+
+  validates_uniqueness_of :nombre, :case_sensitive => false
+
+  validates_numericality_of :precio_unitario,
+    :less_than => 9999999, :greater_than_or_equal_to => 1
+
+  validates_presence_of :stock_real, :stock_critico
+
+  validates_numericality_of :stock_real,
+    :less_than => 99999, :greater_than_or_equal_to => 1,
+    :unless => "stock_real.nil?"
+
+  validates_numericality_of :stock_critico,
+    :less_than => 99999, :greater_than_or_equal_to => 1,
+    :unless => "stock_critico.nil?"
+
+  validate :diferencia_stocks
+
+  protected #-----
+
+  def diferencia_stocks
+    unless stock_critico.nil? || stock_real.nil?
+      errors.add(:stock_real, "debe ser mayor que el stock crítico") if
+      stock_critico >= stock_real
+    end
+  end
+
+  def downcase_nombre
+    self.nombre.downcase!
+  end
 
 end
