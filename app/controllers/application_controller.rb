8da4cd0283048f9b8f29ed8e3981a7ce63d2a29b
Bernardo Arancibia <debiano@gmail.com>
2010-11-05 13:37:43 -0300

    dejo la sesión falsa como administrador true por defecto

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
+#encoding: utf-8
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
   protected #-------
 
   def es_administrador?
-    @administrador = false
+    # aquí debe ir el accesso a bd y variable de sesion
+    # SESSION[:usuario_id]
+    @administrador = true
   end
 
   def administrador_pages
