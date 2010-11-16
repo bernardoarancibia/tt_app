c7145a1d99f3422a85459d7f1168d02ca71555f4
Sebastian Machuca <sebastian.machuca@alumnos.usm.cl>
2010-11-16 15:51:02 -0300

    subo carpetas

--- app/controllers/ventas_controller.rb
+++ app/controllers/ventas_controller.rb
@@ -1,2 +1,49 @@
+#encoding: utf-8
 class VentasController < ApplicationController
+  
+before_filter :find_venta, :only => [:show, :edit, :update, :destroy]
+
+  def index
+    @ventas = Venta.order(:updated_at)
+  end
+
+  def show
+  end
+
+  def new
+    @venta = Venta.new
+  end
+
+  def create
+    @venta = Venta.new(params[:venta])
+    if @venta.save
+      redirect_to :ventas, :notice => "La venta se creó exitosamente."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @venta.update_attributes(params[:venta])
+      redirect_to :ventas, :notice => "La venta se actualizó exitosamente."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @venta.destroy
+    redirect_to :ventas, :notice => "La venta se eliminó exitosamente."
+  end
+
+  private #----------
+
+  def find_venta
+    @venta = Venta.find_by_id(params[:id])
+  end
+
+
 end
