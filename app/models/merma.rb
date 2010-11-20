2d75c1b41c23eb061b72ff2f30427ebe1508ee87
Sebastian Machuca <sebastian.machuca@alumnos.usm.cl>
2010-11-20 17:11:26 -0300

    agrego modificacion y validacion de mermas

--- app/models/merma.rb
+++ app/models/merma.rb
@@ -1,11 +1,32 @@
+#encoding:utf-8
 class Merma < ActiveRecord::Base
 
   #---Dependencia Existencial---
-  belongs_to :producto
+  belongs_to :producto  
+  
+  #---Crear campo nombre_de_producto---
+  attr_accessor :nombre_de_producto
 
   #---Atributos Accesibles---
-  attr_accessible :producto_id, :cantidad, :tipo_merma, :comentario
+  attr_accessible :producto_id, :cantidad, :tipo_merma, :comentario, :nombre_de_producto
 
+  #---validar producto
+  validate :validar_producto
+  #before_validation :validar_producto
+  
+  
   #---Validaciones---
   validates_numericality_of :cantidad, :tipo_merma
+  validates_presence_of :nombre_de_producto
+
+
+  def validar_producto
+    producto = Producto.find_by_nombre(self.nombre_de_producto.downcase)
+    if producto.nil?
+      errors.add(:nombre_de_producto, "no es válido")
+    else
+      self.producto = producto
+    end
+  end
+
 end
