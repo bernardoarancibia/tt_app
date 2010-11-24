3b78c3bdba28558a43ee8e55397f81091b0ba964
Bernardo Arancibia <debiano@gmail.com>
2010-11-24 19:49:42 -0300

    agrego atributo virtual valor_interes_entero

--- app/models/credito.rb
+++ app/models/credito.rb
@@ -3,4 +3,15 @@ class Credito < ActiveRecord::Base
   #---Dependencia Existencial---
   belongs_to :cliente
   belongs_to :venta
+
+  attr_accessor :valor_interes_entero
+
+  def valor_interes_entero= valor
+    write_attribute(:valor_interes, (valor.to_f / 100) +1)
+  end
+
+  def self.valor_interes_entero
+    (self.valor_interes * 100) -1
+  end
+
 end
