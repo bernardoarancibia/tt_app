7eff3c5721e02a71ad3979a35caeb140387fbcf8
Bernardo Arancibia <debiano@gmail.com>
2010-12-07 18:57:46 -0300

    solución al select de pedidos pendientes y aceptados con array y métodos para cada uno

--- app/controllers/pedidos_controller.rb
+++ app/controllers/pedidos_controller.rb
@@ -1,9 +1,9 @@
 class PedidosController < ApplicationController
   def index
     if params[:aceptado] == "true"
-      @pedidos = Pedido.find_by_sql("SELECT * FROM pedidos, ventas WHERE pedidos.id = ventas.pedido_id")
+      @pedidos = find_pedidos_aceptados
     elsif params[:aceptado] == "false"
-      @pedidos = Pedido.find_by_sql("SELECT * FROM pedidos, ventas WHERE ventas.pedido_id <> pedidos.id")
+      @pedidos = find_pedidos_pendientes
     elsif params[:cliente_id]
       @pedidos = Pedido.where(:cliente_id => params[:cliente])
     else
@@ -19,6 +19,26 @@ class PedidosController < ApplicationController
     @pedido = Pedido.find(params[:id])
     @pedido.destroy
     redirect_to :pedidos, :notice => 'Se ha eliminado el pedido correctamente'
+  end
+
+  private #-------
+
+  def find_pedidos_aceptados
+    pedidos = Pedido.all
+    pedidos_aceptados = []
+    pedidos.each do |p|
+       pedidos_aceptados << p if p.aceptado?
+    end
+    return pedidos_aceptados
+  end
+
+  def find_pedidos_pendientes
+    pedidos = Pedido.all
+    pedidos_pendientes = []
+    pedidos.each do |p|
+      pedidos_pendientes << p unless p.aceptado?
+    end
+    return pedidos_pendientes
   end
 
 end
